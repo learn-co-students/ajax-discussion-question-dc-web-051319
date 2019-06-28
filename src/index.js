0b481f99a57fc9b092478f13204bc82bae354ead
Matt Williams <matt.seongjun@gmail.com>
2019-06-28 09:25:01 -0400

    fetch working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const fullname = document.getElementById("fullname");
+const addButton = document.querySelector(".btn-primary")
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
+    console.log("CONTENT LOADED!");
+    addButton.addEventListener("click", addPersonHandler);
 });
+
+function renderPerson(person) {
+    //append data to nodes
+};
+
+function addPersonHandler(event) {
+    fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(person => {
+            renderPerson(person.results) 
+        });
+};
